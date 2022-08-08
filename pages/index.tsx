0f6015b93076e7e0fe03b515b57ef5c00857484e
Brian Lai <brianlai30@outlook.com>
2022-08-08 11:34:11 -0700

    Update all equips, add presets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,62 @@
 import type { NextPage } from 'next'
 import Image, { StaticImageData } from 'next/image'
-import { format, formatDistanceToNow } from 'date-fns'
-import { images } from '../public/img'
-import { Octokit } from 'octokit'
 import { useEffect, useState } from 'react'
+import { format, formatDistanceToNow } from 'date-fns'
+import { Octokit } from 'octokit'
+import { images } from '../public/img'
+import { RadioGroup } from '@headlessui/react'
 
 interface Equip {
   name: string
   image: StaticImageData
+  alt?: [StaticImageData | null, StaticImageData | null]
+}
+
+// prettier-ignore
+const equips: Equip[] = [
+  { name: 'Ring 1', image: images.ring0, alt: [null, images.ring0_1] },
+  { name: 'Ring 2', image: images.ring1, alt: [images.ring1_0, null] },
+  { name: 'Ring 3', image: images.ring2 },
+  { name: 'Ring 4', image: images.ring3, alt: [images.ring3_0, images.ring3_1] },
+  { name: 'Pocket', image: images.pocket },
+
+  { name: 'Pendant 1', image: images.pendant0 },
+  { name: 'Pendant 2', image: images.pendant1, alt: [null, images.pendant1_1] },
+  { name: 'Weapon', image: images.weapon },
+  { name: 'Belt', image: images.belt },
+  { name: 'Medal', image: images.medal },
+
+  { name: 'Hat', image: images.hat },
+  { name: 'Face', image: images.face, alt: [images.face_0, null] },
+  { name: 'Eye', image: images.eye },
+  { name: 'Top', image: images.top },
+  { name: 'Bottom', image: images.bottom },
+
+  { name: 'Earring', image: images.earring },
+  { name: 'Shoulder', image: images.shoulder },
+  { name: 'Glove', image: images.glove },
+  { name: 'Cape', image: images.cape },
+  { name: 'Shoe', image: images.shoe },
+
+  { name: 'Emblem', image: images.emblem },
+  { name: 'Badge', image: images.badge },
+  { name: 'Secondary', image: images.secondary },
+  { name: 'Heart', image: images.heart },
+]
+
+const presetOptions: { name: string; index: number }[] = [
+  { name: 'Default', index: 0 },
+  { name: 'Mesos Obtained', index: 1 },
+  { name: 'Item Drop Rate', index: 2 },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
 }
 
 const Home: NextPage = () => {
   const [updatedDate, setupdatedDate] = useState<Date>()
-
-  const equips: Equip[] = [
-    { name: 'Ring 1', image: images.ring0 },
-    { name: 'Ring 2', image: images.ring1 },
-    { name: 'Ring 3', image: images.ring2 },
-    { name: 'Ring 4', image: images.ring3 },
-    { name: 'Pendant 1', image: images.pendant0 },
-    { name: 'Pendant 2', image: images.pendant1 },
-    { name: 'Weapon', image: images.weapon },
-    { name: 'Belt', image: images.belt },
-    { name: 'Hat', image: images.hat },
-    { name: 'Top', image: images.top },
-    { name: 'Bottom', image: images.bottom },
-    { name: 'Shoe', image: images.shoe },
-    { name: 'Face', image: images.face },
-    { name: 'Eye', image: images.eye },
-    { name: 'Shoulder', image: images.shoulder },
-    { name: 'Glove', image: images.glove },
-    { name: 'Emblem', image: images.emblem },
-    { name: 'Earring', image: images.earring },
-    { name: 'Secondary', image: images.secondary },
-    { name: 'Cape', image: images.cape },
-  ]
+  const [preset, setPreset] = useState(presetOptions[0])
 
   useEffect(() => {
     const fetchCommit = async () => {
@@ -54,25 +76,70 @@ const Home: NextPage = () => {
   }, [])
 
   return (
-    <main className="container mx-auto py-5">
-      <div className="p-6 space-y-2">
+    <main className="container mx-auto flex flex-col items-center gap-8 py-12">
+      <div className="space-y-2">
         <h3 className="text-center text-2xl font-bold text-neutral-600 dark:text-neutral-300">
-          Updated {updatedDate ? formatDistanceToNow(updatedDate) : "..."} ago
+          Updated {updatedDate ? formatDistanceToNow(updatedDate) : '...'} ago
         </h3>
         <h3 className="text-center text-neutral-600 dark:text-neutral-300">
-          {updatedDate ? format(updatedDate, 'yyyy-MM-dd HH:mm a') : "..."}
+          {updatedDate ? format(updatedDate, 'yyyy-MM-dd HH:mm a') : '...'}
         </h3>
       </div>
-      <ul className="flex flex-col flex-wrap justify-center gap-1 align-top md:grid-flow-col md:flex-row 2xl:grid 2xl:grid-cols-4 2xl:grid-rows-4">
+
+      <RadioGroup value={preset} onChange={setPreset} className="">
+        <RadioGroup.Label className="sr-only">
+          Choose equipment preset option
+        </RadioGroup.Label>
+        <div className="grid grid-cols-1 sm:grid-cols-3">
+          {presetOptions.map((option) => (
+            <RadioGroup.Option
+              key={option.name}
+              value={option}
+              className={({ checked }) =>
+                classNames(
+                  checked
+                    ? 'bg-neutral-700 text-white dark:bg-neutral-300 dark:text-black'
+                    : 'border-gray-200 bg-white text-gray-900 hover:bg-neutral-300 dark:border-neutral-700 dark:bg-neutral-800 dark:text-gray-100 dark:hover:bg-neutral-700',
+                  'flex select-none items-center justify-center border py-3 px-6 text-sm font-medium uppercase first:rounded-t-md first:border-b-0 last:rounded-b-md last:border-t-0 sm:flex-1 sm:first:rounded-l-md sm:first:rounded-r-none sm:first:border-r-0 sm:first:border-b sm:last:rounded-r-md sm:last:rounded-l-none sm:last:border-l-0 sm:last:border-t'
+                )
+              }
+            >
+              <RadioGroup.Label as="span">{option.name}</RadioGroup.Label>
+            </RadioGroup.Option>
+          ))}
+        </div>
+      </RadioGroup>
+
+      <ul className="2xl: flex flex-col flex-wrap justify-center gap-2 align-top md:grid-flow-col md:flex-row 2xl:grid 2xl:grid-cols-4 2xl:grid-rows-5">
         {equips.map((equip) => (
           <li
             key={equip.name}
-            className="flex flex-col gap-6 rounded-xl px-6 py-4 hover:bg-white hover:shadow-lg hover:dark:bg-neutral-800 hover:dark:shadow-neutral-800/50"
+            className="flex flex-col gap-6 rounded-xl px-6 py-4"
           >
             <p className="text-center text-xl font-medium text-black dark:text-white">
               {equip.name}
             </p>
-            <Image src={equip.image} alt={equip.name} />
+            {equip.image && (
+              <Image
+                src={
+                  preset.index === 0
+                    ? equip.image
+                    : preset.index === 1
+                    ? equip.alt && equip.alt[0]
+                      ? equip.alt[0]
+                      : equip.image
+                    : preset.index === 2
+                    ? equip.alt && equip.alt[1]
+                      ? equip.alt[1]
+                      : equip.image
+                    : equip.image
+                }
+                alt={equip.name}
+                layout="fixed"
+                loader={({ src, quality }) => `${src}?q=${quality || 100}`}
+                placeholder="blur"
+              />
+            )}
           </li>
         ))}
       </ul>
--- public/img/index.tsx
+++ public/img/index.tsx
@@ -2,42 +2,72 @@ import ring0 from './ring0.png'
 import ring1 from './ring1.png'
 import ring2 from './ring2.png'
 import ring3 from './ring3.png'
+import pocket from './pocket.png'
+
 import pendant0 from './pendant0.png'
 import pendant1 from './pendant1.png'
 import weapon from './weapon.png'
 import belt from './belt.png'
+import medal from './medal.png'
+
 import hat from './hat.png'
-import top from './top.png'
-import bottom from './bottom.png'
-import shoe from './shoe.png'
 import face from './face.png'
 import eye from './eye.png'
+import top from './top.png'
+import bottom from './bottom.png'
+
+import earring from './earring.png'
 import shoulder from './shoulder.png'
 import glove from './glove.png'
-import emblem from './emblem.png'
-import earring from './earring.png'
-import secondary from './secondary.png'
 import cape from './cape.png'
+import shoe from './shoe.png'
+
+import emblem from './emblem.png'
+import badge from './badge.png'
+import secondary from './secondary.png'
+import heart from './heart.png'
+
+import ring0_1 from './ring0_1.png'
+import ring1_0 from './ring1_0.png'
+import ring3_0 from './ring3_0.png'
+import ring3_1 from './ring3_1.png'
+import pendant1_1 from './pendant1_1.png'
+import face_0 from './face_0.png'
 
 export const images = {
   ring0,
   ring1,
   ring2,
   ring3,
+  pocket,
+
   pendant0,
   pendant1,
   weapon,
   belt,
+  medal,
+
   hat,
-  top,
-  bottom,
-  shoe,
   face,
   eye,
+  top,
+  bottom,
+
+  earring,
   shoulder,
   glove,
-  emblem,
-  earring,
-  secondary,
   cape,
+  shoe,
+
+  emblem,
+  badge,
+  secondary,
+  heart,
+
+  ring0_1,
+  ring1_0,
+  ring3_0,
+  ring3_1,
+  pendant1_1,
+  face_0,
 }
